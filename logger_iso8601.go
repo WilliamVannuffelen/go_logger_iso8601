56f1087a51110880b5bd9683cfe98dc49966221c
William Vannuffelen <williamvannuffelen@gmail.com>
2022-06-26 17:56:21 +0200

    chore: run gofmt

--- logger_iso8601.go
+++ logger_iso8601.go
@@ -1,13 +1,13 @@
 package logger_iso8601
 
 import (
-	"os"
-	"io"
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"os"
 	"path/filepath"
-	"strings"
 	"strconv"
-  log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 type customFormatter struct {
@@ -21,34 +21,34 @@ func (f *customFormatter) Format(entry *log.Entry) ([]byte, error) {
 	case log.DebugLevel, log.TraceLevel:
 		levelColor = 31 // grey
 	case log.WarnLevel:
-		levelColor = 33  // yellow
+		levelColor = 33 // yellow
 	case log.ErrorLevel, log.FatalLevel, log.PanicLevel:
-		levelColor = 31  // red
+		levelColor = 31 // red
 	default:
-		levelColor = 36  // blue
+		levelColor = 36 // blue
 	}
 
-	return []byte(fmt.Sprintf("%s - %s:%s - [%s] - \x1b[%dm%s\x1b[0m - %s\n", 
-														entry.Time.Format(f.TimestampFormat),
-														filepath.Base(entry.Caller.File),
-														strconv.Itoa(entry.Caller.Line),
-														entry.Caller.Function,
-														levelColor,
-														strings.ToUpper(entry.Level.String()),
-														entry.Message)),
-														nil
+	return []byte(fmt.Sprintf("%s - %s:%s - [%s] - \x1b[%dm%s\x1b[0m - %s\n",
+			entry.Time.Format(f.TimestampFormat),
+			filepath.Base(entry.Caller.File),
+			strconv.Itoa(entry.Caller.Line),
+			entry.Caller.Function,
+			levelColor,
+			strings.ToUpper(entry.Level.String()),
+			entry.Message)),
+		nil
 }
 
 func InitLogger(filePath string) (*log.Logger, error) {
 	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0777)
 	myCustomLogger := &log.Logger{
-		Out: io.MultiWriter(os.Stderr, f),
-		Level: log.InfoLevel,
+		Out:          io.MultiWriter(os.Stderr, f),
+		Level:        log.InfoLevel,
 		ReportCaller: true,
 		Formatter: &customFormatter{log.TextFormatter{
-			FullTimestamp:			true,
-			TimestampFormat: 		"2006-01-02T15:04:05-0700",
-			ForceColors:				true,
+			FullTimestamp:          true,
+			TimestampFormat:        "2006-01-02T15:04:05-0700",
+			ForceColors:            true,
 			DisableLevelTruncation: true,
 		},
 		},
